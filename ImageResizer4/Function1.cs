9633c254d0dd7ef300d85b09aa52fe709af0c74f
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2024-06-23 10:10:44 +0545

    Add project files.

---
+++ ImageResizer4/Function1.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ImageResizer4.Models;
+using ImageResizer4;
+using Azure.Storage.Blobs;
+
+namespace ImageResizer4
+{
+    public  class Function1
+    {
+        [FunctionName("Function1")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+        [Blob("photos", FileAccess.ReadWrite, Connection = Literals.StorageConnectionString)] BlobContainerClient blobContainer,
+        ILogger _logger)
+
+        {
+            _logger?.LogInformation("C# HTTP trigger function processed a request.");
+            //read the body and get request
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            var request = JsonConvert.DeserializeObject<PhotoUploadModel>(body);
+            //new blobName to store:
+            var newId = Guid.NewGuid();
+            var blobName = $"{newId}.jpg";
+            //make sure blob exists:
+            await blobContainer.CreateIfNotExistsAsync();
+            //get blobReference for cloudBlockBlob
+            var cloudBlockBlob = blobContainer.GetBlobClient(blobName);
+            //get the bytes of photofrom request object:
+            var photoBytes = Convert.FromBase64String(request.Photo);
+            //upload byte photo to cloudBLockBlob;
+            using var photoStream = new MemoryStream(photoBytes);
+            await cloudBlockBlob.UploadAsync(photoStream, true);
+            //logger
+            _logger.LogInformation($"successfully upload {newId}");
+            return new OkObjectResult(newId);
+        }
+    }
+}
---
+++ ImageResizer4/Literals.cs
@@ -0,0 +1,8 @@
+﻿namespace ImageResizer4
+{
+    public static class Literals
+    {
+        public const string StorageConnectionString = nameof(StorageConnectionString);
+        public const string CosmosDBConnection = nameof(CosmosDBConnection);
+    }
+}
